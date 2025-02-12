e1d7df7130289a8e11799984fdcdab654032df6b
Neeraj singh rajput <neeraj@antheminfotech.in>
2025-02-12 13:23:21 +0530

    live

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const PrivateRoute = ({ children, allowedUserTypes }) => {
   const { user } = useAuth();
 
   // console.log("User:", user);
+
   // console.log("Allowed User Types:", allowedUserTypes);
   // console.log(
   //   "User Type Check:",
