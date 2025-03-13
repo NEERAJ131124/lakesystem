af9ff2e811af11fcaaacad35c39e677b81a80673
Neeraj singh rajput <neeraj@antheminfotech.in>
2025-03-13 16:03:40 +0530

    live

--- src/constants/APIs.js
+++ src/constants/APIs.js
@@ -1,3 +1,2 @@
 // export const baseUrl = "http://localhost:5000";
-export const baseUrl =
-  "https://deploymentest-djdabackgheebtev.canadacentral-01.azurewebsites.net";
+export const baseUrl = "https://carplake-backend.azurewebsites.net";
