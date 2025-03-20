3a455d3bd8307b98b4b8c7c0b2b9610421737ab2
saurabh yadav <saurabh@antheminfotech.com>
2025-03-20 14:36:48 +0530

    api url chnage

--- src/constants/APIs.js
+++ src/constants/APIs.js
@@ -1,3 +1,5 @@
 // export const baseUrl = "http://localhost:5000";
 
-export const baseUrl = "https://carplake-backend.azurewebsites.net";
+// export const baseUrl = "https://carplake-backend.azurewebsites.net";
+export const baseUrl = "https://carplake-be-a2dfcebafdbtgmfv.canadacentral-01.azurewebsites.net";
+
